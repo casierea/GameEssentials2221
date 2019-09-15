813577fd9e6558e40d47ec7b099d5933f61e4ab4
Unknown <dancingmongoose@gmail.com>
2019-09-15 14:34:18 -0600

    BirdCircleWorks

---
+++ GameEssentialsProject/Assets/Scripts/UpDateAnims.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDateAnims : MonoBehaviour
+{
+    private Animator anims;
+    public float Speed;
+
+    private void Start()
+    {
+        anims = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        anims.SetFloat("Speed", Speed);
+    }
+}

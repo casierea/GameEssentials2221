c098517e0dcaa124fbbbe5b2f968b2655fb5ce6c
Unknown <dancingmongoose@gmail.com>
2019-09-29 18:01:13 -0600

    Text mesh pro. Trying to get scrolling text.
    Canvas moves around and text wont stay put when play in full screen

---
+++ GameEssentialsProject/Assets/Scripts/MarqueeScroller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class MarqueeScroller : MonoBehaviour {
+
+    public TextMeshProUGUI theText;
+    public float marqueeSpeed = 5f;
+
+    private TextMeshProUGUI copyText;
+    private RectTransform rectTextTransform;
+  
+    void Awake () {
+        rectTextTransform = theText.GetComponent<RectTransform>();
+        
+        copyText = Instantiate(theText);
+        RectTransform cloneRectTransform = copyText.GetComponent<RectTransform>();
+        cloneRectTransform.SetParent(rectTextTransform);
+        cloneRectTransform.anchorMin = new Vector2(1, 1);
+        cloneRectTransform.localPosition = new Vector3(theText.preferredWidth, 1, cloneRectTransform.position.z);
+        cloneRectTransform.localScale = new Vector3(1, 1, 1);
+        copyText.text = theText.text;  
+
+    }
+
+    private IEnumerator Start()
+    {
+        
+        float width = theText.preferredWidth;      
+        Vector3 startPosition = rectTextTransform.localPosition;
+
+        float scrollingPosition = 0;
+
+        while (true)
+        {
+            rectTextTransform.localPosition = new Vector3(-scrollingPosition % width, startPosition.y, startPosition.z);
+            scrollingPosition += marqueeSpeed * 10 * Time.deltaTime;
+            yield return null;
+        }      
+    }
+}

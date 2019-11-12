33aa994023851e7044a7d7d1d4ca5f4dff15513e
Unknown <dancingmongoose@gmail.com>
2019-11-12 13:37:14 -0700

    objs placed. particles, fog, stars, smoke,fire

--- GameEssentialsProject/Assets/Scripts/MarqueeScroller.cs
+++ GameEssentialsProject/Assets/Scripts/MarqueeScroller.cs
@@ -2,40 +2,41 @@
 using UnityEngine;
 using TMPro;
 
-public class MarqueeScroller : MonoBehaviour {
+public class MarqueeScroller : MonoBehaviour
+{
 
-    public TextMeshProUGUI theText;
-    public float marqueeSpeed = 5f;
+ //public TextMeshProUGUI theText;
+ // public float marqueeSpeed = 5f;
 
-    private TextMeshProUGUI copyText;
-    private RectTransform rectTextTransform;
-  
-    void Awake () {
-        rectTextTransform = theText.GetComponent<RectTransform>();
-        
-        copyText = Instantiate(theText);
-        RectTransform cloneRectTransform = copyText.GetComponent<RectTransform>();
-        cloneRectTransform.SetParent(rectTextTransform);
-        cloneRectTransform.anchorMin = new Vector2(1, 1);
-        cloneRectTransform.localPosition = new Vector3(theText.preferredWidth, 1, cloneRectTransform.position.z);
-        cloneRectTransform.localScale = new Vector3(1, 1, 1);
-        copyText.text = theText.text;  
+ // private TextMeshProUGUI copyText;
+ // private RectTransform rectTextTransform;
 
-    }
+ // void Awake () {
+ //   rectTextTransform = theText.GetComponent<RectTransform>();
 
-    private IEnumerator Start()
-    {
-        
-        float width = theText.preferredWidth;      
-        Vector3 startPosition = rectTextTransform.localPosition;
+ //  copyText = Instantiate(theText);
+ // RectTransform cloneRectTransform = copyText.GetComponent<RectTransform>();
+ // cloneRectTransform.SetParent(rectTextTransform);
+ // cloneRectTransform.anchorMin = new Vector2(1, 1);
+ // cloneRectTransform.localPosition = new Vector3(theText.preferredWidth, 1, cloneRectTransform.position.z);
+ //cloneRectTransform.localScale = new Vector3(1, 1, 1);
+ // copyText.text = theText.text;  
 
-        float scrollingPosition = 0;
+ // }
 
-        while (true)
-        {
-            rectTextTransform.localPosition = new Vector3(-scrollingPosition % width, startPosition.y, startPosition.z);
-            scrollingPosition += marqueeSpeed * 10 * Time.deltaTime;
-            yield return null;
-        }      
-    }
+ //private IEnumerator Start()
+ // {
+
+ // float width = theText.preferredWidth;      
+ // Vector3 startPosition = rectTextTransform.localPosition;
+
+ // float scrollingPosition = 0;
+
+ //while (true)
+ //{
+ //  rectTextTransform.localPosition = new Vector3(-scrollingPosition % width, startPosition.y, startPosition.z);
+ //  scrollingPosition += marqueeSpeed * 10 * Time.deltaTime;
+ //  yield return null;
+ //}      
+ //}
 }
